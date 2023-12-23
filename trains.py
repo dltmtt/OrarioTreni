d32547833b9ae6c28f5171ac101ecf3938549e63
Matteo Delton <deltonmatteo@gmail.com>
2023-12-23 12:14:49 +0100

    Format with Black

--- trains.py
+++ trains.py
@@ -1,5 +1,5 @@
-__version__ = '0.1'
-__author__ = 'Matteo Delton'
+__version__ = "0.1"
+__author__ = "Matteo Delton"
 
 import logging
 from argparse import ArgumentParser, BooleanOptionalAction
@@ -21,29 +21,26 @@ class Train:
         self.departure_date = departure_date
 
     def __str__(self):
-        if hasattr(self, 'number_changes'):
-            numbers = f'{self.train_number}{
-                "/".join(c['new_train_number'] for c in self.number_changes)}'
-        else:
-            numbers = self.train_number
+        numbers = self.train_number
+        if hasattr(self, "number_changes"):
+            numbers += "/".join(c["new_train_number"] for c in self.number_changes)
 
-        if hasattr(self, 'category'):
-            return f'{self.category} {numbers}'
+        if hasattr(self, "category"):
+            return f"{self.category} {numbers}"
         else:
-            return f'{numbers}'
+            return f"{numbers}"
 
     def __repr__(self):
-        return f'Train({self.departure_station}, {self.train_number}, {self.departure_date})'
+        return f"Train({self.departure_station}, {self.train_number}, {self.departure_date})"
 
 
 def show_statistics():
     s = API.get_statistics()
 
     print(
-        f'Treni in circolazione da mezzanotte: {s['trains_since_midnight']}\n'
-        f'Treni in circolazione ora: {s['trains_running']}\n'
-        f'{S.DIM}Ultimo aggiornamento: {s['last_update'].strftime("%T")}{
-            S.NORMAL}'
+        f"Treni in circolazione da mezzanotte: {s['trains_since_midnight']}\n"
+        f"Treni in circolazione ora: {s['trains_running']}\n"
+        f"{S.DIM}Ultimo aggiornamento: {s['last_update'].strftime('%T')}{S.NORMAL}"
     )
 
 
@@ -51,17 +48,14 @@ def choose_station(station):
     s = API.get_stations_matching_prefix(station)
 
     if not s:
-        print('Nessuna stazione trovata')
+        print("Nessuna stazione trovata")
         return
 
     if len(s) == 1:
-        return s[0]['name'], s[0]['id']
+        return s[0]["name"], s[0]["id"]
 
-    guesses = tuple((s['name'], s['id']) for s in s)
-    choice = inquirer.list_input(
-        message='Seleziona la stazione',
-        choices=guesses
-    )
+    guesses = tuple((s["name"], s["id"]) for s in s)
+    choice = inquirer.list_input(message="Seleziona la stazione", choices=guesses)
 
     name = next(s[0] for s in guesses if s[1] == choice)
     id = choice
@@ -70,89 +64,82 @@ def choose_station(station):
 
 
 def _process_train(t):
-    train = Train(t['origin_id'], t['number'], t['departure_date'])
-    train.category = t['category']
+    train = Train(t["origin_id"], t["number"], t["departure_date"])
+    train.category = t["category"]
 
-    if t['departed_from_origin']:
-        if t['delay'] > 0:
+    if t["departed_from_origin"]:
+        if t["delay"] > 0:
             delay = F.red(f'{t["delay"]:+}')
-        elif t['delay'] < 0:
+        elif t["delay"] < 0:
             delay = F.green(f'{t["delay"]:+}')
         else:
-            delay = 'In orario'
+            delay = "In orario"
     else:
-        delay = 'Non partito'
+        delay = "Non partito"
 
-    if t['track'] is None:
-        track = t['scheduled_track'] or ''
-    elif t['track'] == t['scheduled_track']:
-        track = F.blue(t['track'])
+    if t["track"] is None:
+        track = t["scheduled_track"] or ""
+    elif t["track"] == t["scheduled_track"]:
+        track = F.blue(t["track"])
     else:
         # Even if scheduled track is None…
-        track = F.magenta(t['track'])
+        track = F.magenta(t["track"])
 
     # Update with more accurate data
-    tp = API.get_train_progress(
-        t['origin_id'], t['number'], t['departure_date'])
+    tp = API.get_train_progress(t["origin_id"], t["number"], t["departure_date"])
 
     if tp:
-        train.number_changes = tp['train_number_changes']
+        train.number_changes = tp["train_number_changes"]
 
         s = None
         o = None
-        for stop in tp['stops']:
-            if stop['station_id'] == station_id and s is None:
+        for stop in tp["stops"]:
+            if stop["station_id"] == station_id and s is None:
                 s = stop
-            if stop['station_id'] == t['origin_id'] and o is None:
+            if stop["station_id"] == t["origin_id"] and o is None:
                 o = stop
             if s is not None and o is not None:
                 break
 
         assert s is not None and o is not None
 
-        if t['departed_from_origin']:
-            if tp['delay'] > 0:
+        if t["departed_from_origin"]:
+            if tp["delay"] > 0:
                 delay = F.red(f'{tp["delay"]:+}')
-            elif tp['delay'] < 0:
+            elif tp["delay"] < 0:
                 delay = F.green(f'{tp["delay"]:+}')
             else:
-                delay = 'In orario'
-        elif s['departure_track']:
-            delay = 'In stazione'  # Pronto a sfrecciare
-        elif o['departure_track']:
-            delay = 'In stazione'  # Quella di origine
+                delay = "In orario"
+        elif s["departure_track"]:
+            delay = "In stazione"  # Pronto a sfrecciare
+        elif o["departure_track"]:
+            delay = "In stazione"  # Quella di origine
         else:
-            delay = 'Non partito'  # E non si sa dov'è
+            delay = "Non partito"  # E non si sa dov'è
 
-        if s['departure_track'] is None:
+        if s["departure_track"] is None:
             # Assuming that the trains departs from the same track
-            if s['arrival_track'] is not None:
-                if s['arrival_track'] == s['scheduled_arrival_track']:
-                    track = F.blue(s['arrival_track'])
+            if s["arrival_track"] is not None:
+                if s["arrival_track"] == s["scheduled_arrival_track"]:
+                    track = F.blue(s["arrival_track"])
                 else:
-                    track = F.magenta(s['arrival_track'])
+                    track = F.magenta(s["arrival_track"])
             else:
-                if s['scheduled_departure_track'] is not None:
-                    track = s['scheduled_departure_track']
+                if s["scheduled_departure_track"] is not None:
+                    track = s["scheduled_departure_track"]
                 else:
                     # Not enough data
-                    track = ''
-        elif s['departure_track'] == s['scheduled_departure_track']:
-            track = F.blue(s['departure_track'])
+                    track = ""
+        elif s["departure_track"] == s["scheduled_departure_track"]:
+            track = F.blue(s["departure_track"])
         else:
             # Even if scheduled track is None…
-            track = F.magenta(s['departure_track'])
+            track = F.magenta(s["departure_track"])
 
-    res = [
-        train,
-        t['destination'],
-        t['departure_time'].strftime('%H:%M'),
-        delay,
-        track
-    ]
+    res = [train, t["destination"], t["departure_time"].strftime("%H:%M"), delay, track]
 
     # Dim the row if the train has already left the station
-    if t['departed_from_station']:
+    if t["departed_from_station"]:
         for i, r in enumerate(res):
             res[i] = S.dim(r)
 
@@ -165,21 +152,15 @@ def _process_train(t):
 
 
 def show_departures(station_name, station_id, dt, limit):
-    print(S.bold(f'Partenze da {station_name}'))
+    print(S.bold(f"Partenze da {station_name}"))
 
     departing_trains = API.get_departures(station_id, dt, limit)
     if not departing_trains:
-        print('Nessun treno in partenza nei prossimi 90 minuti.')
+        print("Nessun treno in partenza nei prossimi 90 minuti.")
         return
 
     table = PrettyTable()
-    table.field_names = [
-        'Treno',
-        'Destinazione',
-        'Partenza',
-        'Ritardo',
-        'Binario'
-    ]
+    table.field_names = ["Treno", "Destinazione", "Partenza", "Ritardo", "Binario"]
 
     with ThreadPoolExecutor(max_workers=len(departing_trains)) as executor:
         for t in executor.map(_process_train, departing_trains):
@@ -188,83 +169,80 @@ def show_departures(station_name, station_id, dt, limit):
     print(table)
 
 
-if __name__ == '__main__':
-    ap = ArgumentParser(description='Get information about trains in Italy')
+if __name__ == "__main__":
+    ap = ArgumentParser(description="Get information about trains in Italy")
 
     ap.add_argument(
-        '-v',
-        '--version',
-        action='version',
-        version=f'%(prog)s {__version__}'
+        "-v", "--version", action="version", version=f"%(prog)s {__version__}"
     )
     ap.add_argument(
-        '-d',
-        '--departures',
-        metavar='STATION',
+        "-d",
+        "--departures",
+        metavar="STATION",
         type=str,
-        help='show departures from a station'
+        help="show departures from a station",
     )
     ap.add_argument(
-        '-a',
-        '--arrivals',
-        metavar='STATION',
+        "-a",
+        "--arrivals",
+        metavar="STATION",
         type=str,
-        help='show arrivals to a station'
+        help="show arrivals to a station",
     )
     ap.add_argument(
-        '-s',
-        '--solutions',
-        metavar=('DEPARTURE', 'ARRIVAL'),
+        "-s",
+        "--solutions",
+        metavar=("DEPARTURE", "ARRIVAL"),
         type=str,
         nargs=2,
-        help='show journey solutions from DEPARTURE to ARRIVAL'
+        help="show journey solutions from DEPARTURE to ARRIVAL",
     )
     ap.add_argument(
-        '--date',
+        "--date",
         metavar="YYYY-MM-DD",
         type=str,
-        help='date to use for the other actions (defaults to today)'
+        help="date to use for the other actions (defaults to today)",
     )
     ap.add_argument(
-        '--time',
+        "--time",
         metavar="HH:MM",
         type=str,
-        help='time to use for the other actions (defaults to now)'
+        help="time to use for the other actions (defaults to now)",
     )
     ap.add_argument(
-        '--limit',
-        metavar='N',
+        "--limit",
+        metavar="N",
         type=int,
-        help='limit the number of results to N (defaults to 10)',
-        default=10
+        help="limit the number of results to N (defaults to 10)",
+        default=10,
     )
     ap.add_argument(
-        '--stats',
+        "--stats",
         action=BooleanOptionalAction,
-        help='show/don\'t show stats (defaults to True)',
-        default=True
+        help="show/don't show stats (defaults to True)",
+        default=True,
     )
     ap.add_argument(
-        '--log-level',
-        metavar='LEVEL',
+        "--log-level",
+        metavar="LEVEL",
         type=str,
-        help='set the logging level (defaults to WARNING)',
-        default='WARNING'
+        help="set the logging level (defaults to WARNING)",
+        default="WARNING",
     )
 
     ap.epilog = (
-        'Departures and arrivals show trains from/to'
-        'the selected station in a range from 15 minutes before'
-        'to 90 minutes after the selected time.'
+        "Departures and arrivals show trains from/to"
+        "the selected station in a range from 15 minutes before"
+        "to 90 minutes after the selected time."
     )
 
     args = ap.parse_args()
 
     # Check if the log level is validf
-    if args.log_level not in ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'):
+    if args.log_level not in ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"):
         print(
-            f'Invalid log level: {F.red(args.log_level)} '
-            '(valid values are: DEBUG, INFO, WARNING, ERROR, CRITICAL).'
+            f"Invalid log level: {F.red(args.log_level)} "
+            "(valid values are: DEBUG, INFO, WARNING, ERROR, CRITICAL)."
         )
 
         exit(1)
@@ -275,12 +253,12 @@ if __name__ == '__main__':
         show_statistics()
 
     if args.date:
-        date_ = datetime.strptime(args.date, '%Y-%m-%d').date()
+        date_ = datetime.strptime(args.date, "%Y-%m-%d").date()
     else:
         date_ = datetime.now().date()
 
     if args.time:
-        time_ = datetime.strptime(args.time, '%H:%M').time()
+        time_ = datetime.strptime(args.time, "%H:%M").time()
     else:
         time_ = datetime.now().time()
 
--- viaggiatreno.py
+++ viaggiatreno.py
@@ -1,5 +1,5 @@
-__version__ = '0.1'
-__author__ = 'Matteo Delton'
+__version__ = "0.1"
+__author__ = "Matteo Delton"
 
 from datetime import date, datetime, time
 
@@ -21,7 +21,7 @@ class ViaggiaTrenoAPIWrapper:
     Train numbers are strings because they can contain letters (e.g. Urb)
     """
 
-    BASE_URI = 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno'
+    BASE_URI = "http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno"
 
     @classmethod
     def _get(cls, endpoint, *args):
@@ -31,18 +31,18 @@ class ViaggiaTrenoAPIWrapper:
 
         print(requests.utils.requote_uri(url))
 
-        return r.json() if 'json' in r.headers['Content-Type'] else r.text
+        return r.json() if "json" in r.headers["Content-Type"] else r.text
 
     @classmethod
     def get_statistics(cls):
         """Return statistics about trains for today."""
         timestamp = int(datetime.now().timestamp() * 1000)
-        r = cls._get('statistiche', timestamp)
+        r = cls._get("statistiche", timestamp)
 
         statistics = {
-            'trains_since_midnight': r['treniGiorno'],
-            'trains_running': r['treniCircolanti'],
-            'last_update': Utils.from_ms_timestamp(r['ultimoAggiornamento'])
+            "trains_since_midnight": r["treniGiorno"],
+            "trains_running": r["treniCircolanti"],
+            "last_update": Utils.from_ms_timestamp(r["ultimoAggiornamento"]),
         }
 
         return statistics
@@ -50,14 +50,11 @@ class ViaggiaTrenoAPIWrapper:
     @classmethod
     def get_stations_matching_prefix(cls, prefix):
         """Return a list of stations starting with the given text."""
-        r = cls._get('cercaStazione', prefix)
+        r = cls._get("cercaStazione", prefix)
 
         stations = []
         for s in r:
-            stations.append({
-                'name': s['nomeLungo'],
-                'id': s['id']
-            })
+            stations.append({"name": s["nomeLungo"], "id": s["id"]})
 
         return stations
 
@@ -68,27 +65,31 @@ class ViaggiaTrenoAPIWrapper:
             dt = datetime.now()
 
         dep_time = Utils.to_string(dt)
-        r = cls._get('partenze', station_id, dep_time)
+        r = cls._get("partenze", station_id, dep_time)
 
         departures = []
         for i, d in enumerate(r):
             if i == limit:
                 break
 
-            departures.append({
-                'category': d['categoriaDescrizione'].strip(),
-                'number': d['numeroTreno'],
-                'departure_date': Utils.from_ms_timestamp(d['dataPartenzaTreno']).date(),
-                'origin_id': d['codOrigine'],
-                'destination': d['destinazione'],
-                'scheduled_track': d['binarioProgrammatoPartenzaDescrizione'],
-                'track': d['binarioEffettivoPartenzaDescrizione'],
-                'departure_time': Utils.from_ms_timestamp(d['orarioPartenza']),
-                'departed_from_origin': not d['nonPartito'],
-                'departed_from_station': d['inStazione'],
-                'delay': int(d['ritardo']),
-                'warning': d['subTitle']
-            })
+            departures.append(
+                {
+                    "category": d["categoriaDescrizione"].strip(),
+                    "number": d["numeroTreno"],
+                    "departure_date": Utils.from_ms_timestamp(
+                        d["dataPartenzaTreno"]
+                    ).date(),
+                    "origin_id": d["codOrigine"],
+                    "destination": d["destinazione"],
+                    "scheduled_track": d["binarioProgrammatoPartenzaDescrizione"],
+                    "track": d["binarioEffettivoPartenzaDescrizione"],
+                    "departure_time": Utils.from_ms_timestamp(d["orarioPartenza"]),
+                    "departed_from_origin": not d["nonPartito"],
+                    "departed_from_station": d["inStazione"],
+                    "delay": int(d["ritardo"]),
+                    "warning": d["subTitle"],
+                }
+            )
 
         return departures
 
@@ -99,39 +100,43 @@ class ViaggiaTrenoAPIWrapper:
             dt = datetime.now()
 
         arr_time = Utils.to_string(dt)
-        r = cls._get('arrivi', station_id, arr_time)
+        r = cls._get("arrivi", station_id, arr_time)
 
         arrivals = []
         for i, a in enumerate(r):
             if i == limit:
                 break
 
-            arrivals.append({
-                'category': a['categoriaDescrizione'].strip(),
-                'number': a['numeroTreno'],
-                'departure_date': Utils.from_ms_timestamp(a['dataPartenzaTreno']).date(),
-                'origin_id': a['codOrigine'],
-                'origin': a['origine'],
-                'scheduled_track': a['binarioProgrammatoArrivoDescrizione'],
-                'track': a['binarioEffettivoArrivoDescrizione'],
-                'arrival_time': Utils.from_ms_timestamp(a['orarioArrivo']),
-                'departed_from_origin': not a['nonPartito'],
-                'departed_from_station': a['inStazione'],
-                'delay': int(a['ritardo']),
-                'warning': a['subTitle']
-            })
+            arrivals.append(
+                {
+                    "category": a["categoriaDescrizione"].strip(),
+                    "number": a["numeroTreno"],
+                    "departure_date": Utils.from_ms_timestamp(
+                        a["dataPartenzaTreno"]
+                    ).date(),
+                    "origin_id": a["codOrigine"],
+                    "origin": a["origine"],
+                    "scheduled_track": a["binarioProgrammatoArrivoDescrizione"],
+                    "track": a["binarioEffettivoArrivoDescrizione"],
+                    "arrival_time": Utils.from_ms_timestamp(a["orarioArrivo"]),
+                    "departed_from_origin": not a["nonPartito"],
+                    "departed_from_station": a["inStazione"],
+                    "delay": int(a["ritardo"]),
+                    "warning": a["subTitle"],
+                }
+            )
 
         return arrivals
 
     @classmethod
     def get_train_info(cls, train_number):
-        r = cls._get('cercaNumeroTreno', train_number)
+        r = cls._get("cercaNumeroTreno", train_number)
 
         train = {
-            'number': r['numeroTreno'],
-            'departure_date': Utils.from_ms_timestamp(r['dataPartenza']).date(),
-            'departure_station_id': r['codLocOrig'],
-            'departure_station': r['descLocOrig']
+            "number": r["numeroTreno"],
+            "departure_date": Utils.from_ms_timestamp(r["dataPartenza"]).date(),
+            "departure_station_id": r["codLocOrig"],
+            "departure_station": r["descLocOrig"],
         }
 
         return train
@@ -145,32 +150,32 @@ class ViaggiaTrenoAPIWrapper:
         origin_id = cls._get_enee_code(origin_id)
         dest_id = cls._get_enee_code(dest_id)
         dt = dt.isoformat()
-        r = cls._get('soluzioniViaggioNew', origin_id, dest_id, dt)
+        r = cls._get("soluzioniViaggioNew", origin_id, dest_id, dt)
 
         solutions = {
-            'origin': r['origine'],
-            'destination': r['destinazione'],
-            'solutions': []
+            "origin": r["origine"],
+            "destination": r["destinazione"],
+            "solutions": [],
         }
 
-        for i, s in enumerate(r['soluzioni']):
+        for i, s in enumerate(r["soluzioni"]):
             if i == limit:
                 break
 
             vehicles = []
-            for v in s['vehicles']:
-                vehicles.append({
-                    'origin': v['origine'],
-                    'destination': v['destinazione'],
-                    'departure_time': datetime.fromisoformat(v['orarioPartenza']),
-                    'arrival_time': datetime.fromisoformat(v['orarioArrivo']),
-                    'category': v['categoriaDescrizione'],
-                    'number': v['numeroTreno']
-                })
+            for v in s["vehicles"]:
+                vehicles.append(
+                    {
+                        "origin": v["origine"],
+                        "destination": v["destinazione"],
+                        "departure_time": datetime.fromisoformat(v["orarioPartenza"]),
+                        "arrival_time": datetime.fromisoformat(v["orarioArrivo"]),
+                        "category": v["categoriaDescrizione"],
+                        "number": v["numeroTreno"],
+                    }
+                )
 
-            solutions['solutions'].append({
-                'vehicles': vehicles
-            })
+            solutions["solutions"].append({"vehicles": vehicles})
 
         return solutions
 
@@ -178,54 +183,62 @@ class ViaggiaTrenoAPIWrapper:
     def get_train_progress(cls, origin_id, train_number, dep_date):
         """Return the progress of a train."""
         dep_date = Utils.to_ms_date_timestamp(dep_date)
-        r = cls._get('andamentoTreno', origin_id,
-                     train_number, dep_date)
+        r = cls._get("andamentoTreno", origin_id, train_number, dep_date)
 
         if not r:
             return None
 
         train_number_changes = []
-        for c in r['cambiNumero']:
-            train_number_changes.append({
-                'new_train_number': c['nuovoNumeroTreno'],
-                'station': c['stazione']
-            })
+        for c in r["cambiNumero"]:
+            train_number_changes.append(
+                {"new_train_number": c["nuovoNumeroTreno"], "station": c["stazione"]}
+            )
 
         stops = []
-        for s in r['fermate']:
-            stops.append({
-                'station_id': s['id'],
-                'station': s['stazione'],
-                'scheduled_arrival_time': Utils.from_ms_timestamp(s['arrivo_teorico']),
-                'arrival_time': Utils.from_ms_timestamp(s['arrivoReale']),
-                'scheduled_departure_time': Utils.from_ms_timestamp(s['partenza_teorica']),
-                'departure_time': Utils.from_ms_timestamp(s['partenzaReale']),
-                'scheduled_arrival_track': s['binarioProgrammatoArrivoDescrizione'],
-                'arrival_track': s['binarioEffettivoArrivoDescrizione'],
-                'scheduled_departure_track': s['binarioProgrammatoPartenzaDescrizione'],
-                'departure_track': s['binarioEffettivoPartenzaDescrizione'],
-            })
+        for s in r["fermate"]:
+            stops.append(
+                {
+                    "station_id": s["id"],
+                    "station": s["stazione"],
+                    "scheduled_arrival_time": Utils.from_ms_timestamp(
+                        s["arrivo_teorico"]
+                    ),
+                    "arrival_time": Utils.from_ms_timestamp(s["arrivoReale"]),
+                    "scheduled_departure_time": Utils.from_ms_timestamp(
+                        s["partenza_teorica"]
+                    ),
+                    "departure_time": Utils.from_ms_timestamp(s["partenzaReale"]),
+                    "scheduled_arrival_track": s["binarioProgrammatoArrivoDescrizione"],
+                    "arrival_track": s["binarioEffettivoArrivoDescrizione"],
+                    "scheduled_departure_track": s[
+                        "binarioProgrammatoPartenzaDescrizione"
+                    ],
+                    "departure_track": s["binarioEffettivoPartenzaDescrizione"],
+                }
+            )
 
         # Check fermateSoppresse, tipoTreno and motivoRitardoPrevalente
         # There's a numeroTreno which is an int here
         progress = {
-            'last_update': Utils.from_ms_timestamp(r['oraUltimoRilevamento']),
-            'last_update_station': s if (s := r['stazioneUltimoRilevamento']) != '--' else None,
-            'number': str(r['numeroTreno']),
-            'departure_date': Utils.from_ms_timestamp(r['dataPartenzaTreno']).date(),
-            'origin_id': r['idOrigine'],
-            'origin': r['origine'],
-            'destination': r['destinazione'],
-            'destination_id': r['idDestinazione'],
-            'train_number_changes': train_number_changes,
-            'departure_time': Utils.from_ms_timestamp(r['orarioPartenza']),
-            'arrival_time': Utils.from_ms_timestamp(r['orarioArrivo']),
-            'departed': not r['nonPartito'],
-            'in_station': r['inStazione'],
-            'delay': int(r['ritardo']),
-            'warning': r['subTitle'] or None,
-            'delay_reason': r['motivoRitardoPrevalente'],
-            'stops': stops
+            "last_update": Utils.from_ms_timestamp(r["oraUltimoRilevamento"]),
+            "last_update_station": s
+            if (s := r["stazioneUltimoRilevamento"]) != "--"
+            else None,
+            "number": str(r["numeroTreno"]),
+            "departure_date": Utils.from_ms_timestamp(r["dataPartenzaTreno"]).date(),
+            "origin_id": r["idOrigine"],
+            "origin": r["origine"],
+            "destination": r["destinazione"],
+            "destination_id": r["idDestinazione"],
+            "train_number_changes": train_number_changes,
+            "departure_time": Utils.from_ms_timestamp(r["orarioPartenza"]),
+            "arrival_time": Utils.from_ms_timestamp(r["orarioArrivo"]),
+            "departed": not r["nonPartito"],
+            "in_station": r["inStazione"],
+            "delay": int(r["ritardo"]),
+            "warning": r["subTitle"] or None,
+            "delay_reason": r["motivoRitardoPrevalente"],
+            "stops": stops,
         }
 
         return progress
@@ -253,7 +266,7 @@ class Utils:
             try:
                 dt = datetime.fromisoformat(date_)
             except ValueError:
-                raise ValueError(f'Invalid date format: {date_}') from None
+                raise ValueError(f"Invalid date format: {date_}") from None
 
         return int(dt.timestamp() * 1000)
 
@@ -264,14 +277,14 @@ class Utils:
         elif isinstance(dt, str):
             dt = datetime.fromisoformat(dt)
 
-        return dt.strftime('%a %b %d %Y %H:%M:%S')
+        return dt.strftime("%a %b %d %Y %H:%M:%S")
 
     @classmethod
     def get_enee_code(cls, station_id):
         if len(station_id) == 6:
-            station_id = int(station_id.lstrip('S0'))
+            station_id = int(station_id.lstrip("S0"))
         elif len(station_id) == 8:
-            station_id = int(station_id.lstrip('830'))
+            station_id = int(station_id.lstrip("830"))
 
         return station_id
 
